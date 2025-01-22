2a1f275ddcb717f108ae4772ac56547d8183bd1d
Halayac-25 <aarka.yare2022@gmail.com>
2025-01-22 02:28:57 -0800

    Fixed build issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/contact";
+import Contact from "./pages/Contact";
 import Service from "./pages/Service";
 
 import { Route , Routes } from "react-router-dom";
